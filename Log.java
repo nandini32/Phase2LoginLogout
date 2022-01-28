2adb999320f6ca0115f112589d16b1c234c99cca
nandini32 <58420860+nandini32@users.noreply.github.com>
2022-01-28 10:32:38 +0530

    Add files via upload

---
+++ Log.java
@@ -0,0 +1,26 @@
+package com.phase2.training;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/log")
+public class Log extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie userId = new Cookie("Userid", request.getParameter("Userid"));
+        response.addCookie(userId);
+     response.sendRedirect("dashboard");  
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
